b1e5c59629facc7d3a14586a870e14ace2e653d1
bbag <bill.baggerman@gmail.com>
2024-04-23 16:58:38 -0400

    Added placeholder for Live Preview page

--- src/views/products/ProductLivePreviewView.vue
+++ src/views/products/ProductLivePreviewView.vue
@@ -1,8 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Card } from '@/components/ui/card'
+import { ResizableHandle, ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable'
+</script>
 
 <template>
-  <div class="p-8">
-    <h1>Live preview...</h1>
+  <div>
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel class="bg-white flex flex-col items-center justify-center">
+        <h1 class="text-3xl font-bold p-8">Live preview</h1>
+        <p>Code snippets will show here... coming soon!</p>
+        <p class="text-slate-500 text-sm">
+          (In the meantime, try dragging and resizing these panels.)
+        </p>
+      </ResizablePanel>
+      <ResizableHandle with-handle />
+      <ResizablePanel class="flex items-center justify-center">
+        3D model will show here... also coming soon!
+      </ResizablePanel>
+    </ResizablePanelGroup>
   </div>
 </template>
 
